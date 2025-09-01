0b04ae8dde802a9db51d0b213b25a1e209fb085b
htikelinn <htikezawlinn@gmail.com>
2025-09-01 17:56:02 +0630

    unuse package delete in SecurityConfig

--- src/main/java/com/example/novalearn/security/SecurityConfig.java
+++ src/main/java/com/example/novalearn/security/SecurityConfig.java
@@ -3,11 +3,9 @@ package com.example.novalearn.security;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
